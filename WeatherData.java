137ae019a870b13b87177f510704e87930276665
Conner Canning <32187629+ConnerCanning@users.noreply.github.com>
2019-08-02 20:16:14 -0700

    Add files via upload

--- WeatherData.java
+++ WeatherData.java
@@ -1,12 +1,16 @@
 
 public class WeatherData {
 
-	public static double temp;
-	public static double humid;
-	public static double rain;
-	public static double windSpeed;
-	public static char windDirection;
-	public static double[] rawValues;
+	public static StringBuilder sb = new StringBuilder();
+	public static String delimiter = "\n";
+	
+	public double temp;
+	public double humid;
+	public double rain;
+	public double windSpeed;
+	public char windDirection;
+	public double[] rawValues;
+	public boolean sensorStatus;
 	
 	public WeatherData(double theTemp, double theHumid, double theRain, double theSpeed, char theDirection, double[] theRawValues) {
 		temp = theTemp;
@@ -15,7 +19,22 @@ public class WeatherData {
 		windSpeed = theSpeed;
 		windDirection = theDirection;
 		rawValues = theRawValues;
+		sensorStatus = true;
 	}
+	
+	public String toString() {
+        sb.append("Temperature: " + temp + " F");
+        sb.append(delimiter);
+        sb.append("Rain Fall: " + rain + " inches");
+        sb.append(delimiter);
+        sb.append("Humidity: " + humid + " %");
+        sb.append(delimiter);
+        sb.append("Wind speed: " + windSpeed + " mph");
+        sb.append(delimiter);
+        sb.append("Wind direction: " + windDirection);
+        return sb.toString();
+    }
+
 }
 	
 
